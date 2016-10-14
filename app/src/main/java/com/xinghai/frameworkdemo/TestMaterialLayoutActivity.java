0871f2238740245d92bc58a46e003ed50c004a05
yuanbaoyu <yuanbaoyu@wangchengiMac.local>
2016-10-14 12:20:29 +0800

    添加sample

---
+++ app/src/main/java/com/xinghai/frameworkdemo/TestMaterialLayoutActivity.java
@@ -0,0 +1,20 @@
+package com.xinghai.frameworkdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created on 16/10/12.
+ * author: yuanbaoyu
+ */
+
+public class TestMaterialLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_materiall_layout);
+    }
+
+}
---
+++ framework/src/main/java/com/xinghai/framework/fragment/BaseFragment.java
@@ -0,0 +1,106 @@
+package com.xinghai.framework.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created on 16/10/13.
+ * author: yuanbaoyu
+ */
+
+public class BaseFragment extends Fragment {
+    private static final String TAG = BaseFragment.class.getSimpleName();
+    private boolean isPrepared;
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initPrepare();
+    }
+
+
+    /**
+     * 第一次onResume中的调用onUserVisible避免操作与onFirstUserVisible操作重复
+     */
+    private boolean isFirstResume = true;
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (isFirstResume) {
+            isFirstResume = false;
+            return;
+        }
+        if (getUserVisibleHint()) {
+            onUserVisible();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (getUserVisibleHint()) {
+            onUserInvisible();
+        }
+    }
+
+    private boolean isFirstVisible = true;
+    private boolean isFirstInvisible = true;
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (isVisibleToUser) {
+            if (isFirstVisible) {
+                isFirstVisible = false;
+                initPrepare();
+            } else {
+                onUserVisible();
+            }
+        } else {
+            if (isFirstInvisible) {
+                isFirstInvisible = false;
+                onFirstUserInvisible();
+            } else {
+                onUserInvisible();
+            }
+        }
+    }
+
+    public synchronized void initPrepare() {
+        if (isPrepared) {
+            onFirstUserVisible();
+        } else {
+            isPrepared = true;
+        }
+    }
+
+    /**
+     * 第一次fragment可见（进行初始化工作）
+     */
+    public void onFirstUserVisible() {
+
+    }
+
+    /**
+     * fragment可见（切换回来或者onResume）
+     */
+    public void onUserVisible() {
+
+    }
+
+    /**
+     * 第一次fragment不可见（不建议在此处理事件）
+     */
+    public void onFirstUserInvisible() {
+
+    }
+
+    /**
+     * fragment不可见（切换掉或者onPause）
+     */
+    public void onUserInvisible() {
+
+    }
+
+}
