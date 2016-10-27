1f2d3b8ab2d5f72ec9bc96c8848c00daa6a554b4
yuanbaoyu <yuanbaoyu@wangchengiMac.local>
2016-10-27 18:06:11 +0800

    添加常用公共工具类

---
+++ framework/src/main/java/com/xinghai/framework/utils/CloseUtils.java
@@ -0,0 +1,52 @@
+package com.xinghai.framework.utils;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ * created at 16/10/27 17:57
+ * @author yuanbaoyu
+ * 关闭相关工具类
+ */
+public class CloseUtils {
+
+    private CloseUtils() {
+        throw new UnsupportedOperationException("u can't instantiate me...");
+    }
+
+    /**
+     * 关闭IO
+     *
+     * @param closeables closeable
+     */
+    public static void closeIO(Closeable... closeables) {
+        if (closeables == null) return;
+        for (Closeable closeable : closeables) {
+            if (closeable != null) {
+                try {
+                    closeable.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * 安静关闭IO
+     *
+     * @param closeables closeable
+     */
+    public static void closeIOQuietly(Closeable... closeables) {
+        if (closeables == null) return;
+        for (Closeable closeable : closeables) {
+            if (closeable != null) {
+                try {
+                    closeable.close();
+                } catch (IOException ignored) {
+                }
+            }
+        }
+    }
+}
